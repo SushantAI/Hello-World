06a688e169d1f9952667951ebcbd592ae2bb4c0f
acp16ssc <acp16ssc@dcs04467.local>
2016-10-04 15:09:38 +0100

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by acp16ssc on 04/10/2016.
+//  Copyright © 2016 acp16ssc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
